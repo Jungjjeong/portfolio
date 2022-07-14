10635f18cdd05018da6912d6ba7d583dd002a076
Jungjjeong <sky990106@dgu.ac.kr>
2022-07-14 18:38:30 +0900

    Design: 색상, 폰트 크기 상수 추가

---
+++ frontend/styles/common.ts
@@ -0,0 +1,44 @@
+/* 
+	import Common from '~/styles/common'
+
+	-- 사용 시 --
+	background: ${Common.colors.main};
+*/
+
+const Common = {
+  colors: {
+    main01: "#0080C8",
+    main02: "#00A7CC",
+
+		main01_light: "#6CC2F2",
+		main02_light: "#6CD5EC",
+
+    gray01: "#333333",
+    gray02: "#4F4F4F",
+    gray03: "#828282",
+    gray04: "#BDBDBD",
+    gray05: "#E0E0E0",
+    gray06: "#F2F2F2",
+
+		white: "#FFFFFF"
+  },
+
+  fontSize: {
+    title: "3.5rem",
+
+    h1: "2.4rem",
+    h2: "2.1rem",
+    h3: "1.9rem",
+
+    b1: "1.8rem",
+    b2: "1.4rem",
+    b3: "1.3rem",
+
+    c1: "1.1rem",
+    c2: "1.2rem",
+
+    n1: "0.8rem",
+  },
+};
+
+export default Common;
