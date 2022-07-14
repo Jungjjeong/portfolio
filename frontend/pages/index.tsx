78898d7761167f63183addd3f65243c59e41c3a1
Jungjjeong <sky990106@dgu.ac.kr>
2022-07-14 18:36:25 +0900

    Chore: storybook setting

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,22 @@
 import type { NextPage } from 'next';
+import styled from '@emotion/styled';
 
 const Home: NextPage = () => {
-  return <div>MAIN PAGE</div>;
+  return (
+    <div>
+      <MainContainer>Main Page</MainContainer>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
+
+const MainContainer = styled.div`
+  height: calc(100vh - 70px);
+`;
+
+const Footer = styled.footer`
+  background-color: tomato;
+  height: 500px;
+`;
