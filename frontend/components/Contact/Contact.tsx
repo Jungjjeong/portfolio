3ee3976bee23acc582f1ad207088930bb82a52a6
Jungjjeong <sky990106@dgu.ac.kr>
2023-03-24 20:33:58 +0900

    refactor: Contact 컴포넌트 tailwindcss 마이그레이션

--- frontend/components/Contact/Contact.tsx
+++ frontend/components/Contact/Contact.tsx
@@ -1,37 +1,38 @@
 import Button from '../Button';
-import S from './Contact.style';
 
 const Contact = () => {
   return (
-    <>
-      <S.ContactWrapper>
-        <S.HeaderText>JUNGJIYOUNG</S.HeaderText>
-        <S.DescriptionText>Web Frontend Developer</S.DescriptionText>
-        <S.ButtonWrapper>
-          <a
-            target="_blank"
-            href="https://github.com/Jungjjeong"
-            rel="noopener noreferrer"
-          >
-            <Button text="GITHUB" />
-          </a>
-          <a
-            target="_blank"
-            href="https://jungjjeong.github.io/"
-            rel="noopener noreferrer"
-          >
-            <Button text="BLOG" />
-          </a>
-          <a
-            target="_blank"
-            href="mailto:sky990106@naver.com"
-            rel="noopener noreferrer"
-          >
-            <Button text="EMAIL" />
-          </a>
-        </S.ButtonWrapper>
-      </S.ContactWrapper>
-    </>
+    <div className="flex flex-col box-border w-full h-auto gap-[10px] py-[180px]">
+      <h2 className="mx-auto w-fit tracking-[3px] text-gray-2 font-bold text-2xl">
+        Jung Jiyoung
+      </h2>
+      <h2 className="mx-auto w-fit tracking-[3px] text-gray-4 font-bold">
+        Web Frontend Developer
+      </h2>
+      <div className="flex gap-[30px] w-fit my-[25px] mx-auto">
+        <a
+          target="_blank"
+          href="https://github.com/Jungjjeong"
+          rel="noopener noreferrer"
+        >
+          <Button text="GITHUB" />
+        </a>
+        <a
+          target="_blank"
+          href="https://jungjjeong.github.io/"
+          rel="noopener noreferrer"
+        >
+          <Button text="BLOG" />
+        </a>
+        <a
+          target="_blank"
+          href="mailto:sky990106@naver.com"
+          rel="noopener noreferrer"
+        >
+          <Button text="EMAIL" />
+        </a>
+      </div>
+    </div>
   );
 };
 
