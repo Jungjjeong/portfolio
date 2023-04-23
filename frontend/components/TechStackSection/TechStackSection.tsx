341d5b08aaa603729df65a9866cdb500f7931266
Jungjjeong <sky990106@dgu.ac.kr>
2023-04-23 18:44:42 +0900

    feat: 기술 스택 소제목 추가

--- frontend/components/TechStackSection/TechStackSection.tsx
+++ frontend/components/TechStackSection/TechStackSection.tsx
@@ -7,15 +7,18 @@ const TechStackSection = () => {
       {techStackData.map((techStack, idx) => {
         const { type, data } = techStack;
         return (
-          <div key={idx}>
-            {/* <h1 className="my-[5px] font-bold">{type}</h1> */}
+          <div
+            key={idx}
+            className="mb-[10px] mb-[15px] flex flex-col gap-[5px]"
+          >
+            <h1 className="my-[5px] font-bold w-fit md:mx-auto">{type}</h1>
             <div className="flex flex-wrap gap-2 items-center md:justify-center">
               {data.map((techData, techIdx) => {
                 const { title, photo } = techData;
                 return (
                   <div
                     key={techIdx}
-                    className="my-[10px] relative w-14 md:w-16 h-14 md:h-16 p-[50x] border-box hover:scale-110 hover:z-10 transition-all duration-75 bg-white cursor-pointer"
+                    className="relative w-14 md:w-16 h-14 md:h-16 p-[50x] border-box hover:scale-110 hover:z-10 transition-all duration-75 bg-white cursor-pointer"
                   >
                     <div
                       className="opacity-0 hover:opacity-100 absolute top-0 left-0 bg-gray/75 z-10 w-full h-full flex items-center justify-center text-sxm text-center font-bold"
