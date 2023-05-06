ae2969c511cea090c01a109c0b3c996d4eada280
Jungjjeong <sky990106@dgu.ac.kr>
2023-05-06 23:52:40 +0900

    style: 모달 스타일 변경

--- frontend/components/Modal/Modal.tsx
+++ frontend/components/Modal/Modal.tsx
@@ -21,11 +21,11 @@ const Modal = ({ modalHandler }: IModal) => {
         className={`opacity-40 fixed inset-y-0 right-0 w-full h-full bg-black z-40 cursor-pointer`}
         onClick={modalHandler}
       />
-      <div className="fixed top-[50vh] left-[50%] translate-x-[-50%] translate-y-[-50%] z-50 w-full h-auto max-w-[400px] px-[15px] border-box">
-        <div className="bg-white rounded-lg w-full pb-[20px] px-[30px] pt-[30px] border-box">
-          <div className="flex flex-col gap-[10px]">
-            <h2 className="text-2xl font-bold">💌 소중한 메일 감사합니다.</h2>
-            <p className="mb-[10px]">빠른 시일 내에 답변을 드릴게요.</p>
+      <div className="fixed top-[50vh] left-[50%] translate-x-[-50%] translate-y-[-50%] z-50 w-full h-auto max-w-[350px] px-[15px] border-box">
+        <div className="bg-white rounded-lg w-full pb-[20px] px-[30px] pt-[25px] border-box">
+          <div className="flex flex-col gap-[8px]">
+            <h2 className="text-2xl font-bold">소중한 연락 감사합니다. 💌</h2>
+            <p className="mb-[10px]">빠른 시일 내에 다시 연락드리겠습니다.</p>
             <Button styleType="dark" text="CLOSE" onClick={modalHandler} />
           </div>
         </div>
