8b65b9033567c09b6049a682d541b4d82fb05d01
Jungjjeong <sky990106@dgu.ac.kr>
2023-05-13 22:09:31 +0900

    feat: mo버전 프로젝트 이미지 swiper 기능 구현

--- frontend/components/EnlargedImage/EnlargedImage.tsx
+++ frontend/components/EnlargedImage/EnlargedImage.tsx
@@ -3,7 +3,9 @@
 import Image, { StaticImageData } from 'next/image';
 import { useEffect, useState } from 'react';
 import { useLockBodyScroll } from '../../hooks';
-import Button from '../Button';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import 'swiper/css';
 
 interface IEnlargedImage {
   imageListData: StaticImageData[];
@@ -21,16 +23,33 @@ const EnlargedImage = ({
   const [selectedImage, setSelectedImage] = useState<StaticImageData | null>(
     null
   );
+  const [swiper, setSwiper] = useState<SwiperCore>();
+  const [swiperOptions, setSwiperOptions] = useState<unknown>(null);
   const [, setLocked] = useLockBodyScroll();
 
   useEffect(() => {
     setLocked(true);
+
+    return () => {
+      setSwiperOptions(null);
+    };
   }, []);
 
   useEffect(() => {
     if (selectedIdx === -1 || !imageListData.length) return;
     setSelectedImage(imageListData[selectedIdx]);
   }, [selectedIdx, imageListData]);
+
+  useEffect(() => {
+    if (!swiperOptions) {
+      setSwiperOptions({
+        className: 'swiper-container',
+        slidesPerView: 1,
+        centeredSlides: true,
+        initialSlide: selectedIdx,
+      });
+    }
+  }, [swiperOptions, selectedIdx]);
 
   const handleClickPrevButton = () => {
     if (selectedIdx <= 0) return;
@@ -51,26 +70,44 @@ const EnlargedImage = ({
         className={`opacity-40 fixed inset-y-0 right-0 w-full h-full bg-black z-40 cursor-pointer`}
         onClick={dimHandler}
       />
-      <div className="max-w-[1150px] w-full h-auto p-[15px] fixed top-[50vh] left-[50%] translate-x-[-50%] translate-y-[-50%] z-50">
-        <Image src={selectedImage} alt="detail Image" />
-        <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full flex justify-between p-[25px]">
-          <button
-            onClick={handleClickPrevButton}
-            className={`${
-              selectedIdx > 0 ? 'visible' : 'invisible'
-            } rounded-full bg-gray-3/50 hover:bg-gray-4/50 w-[35px] h-[35px] text-white text-center`}
-          >
-            ◀️
-          </button>
-          <button
-            onClick={handleClickNextButton}
-            className={`${
-              selectedIdx < imageListData.length - 1 ? 'visible' : 'invisible'
-            } rounded-full bg-gray-3/50 hover:bg-gray-4/50 w-[35px] h-[35px] text-white text-center`}
-          >
-            ▶️
-          </button>
+      {/* PC */}
+      <div className="hidden lg:block">
+        <div className="max-w-[1150px] w-full h-auto p-[15px] fixed top-[50vh] left-[50%] translate-x-[-50%] translate-y-[-50%] z-50">
+          <Image src={selectedImage} alt="detail Image" />
+          <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full flex justify-between p-[25px]">
+            <button
+              onClick={handleClickPrevButton}
+              className={`${
+                selectedIdx > 0 ? 'visible' : 'invisible'
+              } rounded-full bg-gray-3/50 hover:bg-gray-4/50 w-[35px] h-[35px] text-white text-center`}
+            >
+              ◀️
+            </button>
+            <button
+              onClick={handleClickNextButton}
+              className={`${
+                selectedIdx < imageListData.length - 1 ? 'visible' : 'invisible'
+              } rounded-full bg-gray-3/50 hover:bg-gray-4/50 w-[35px] h-[35px] text-white text-center`}
+            >
+              ▶️
+            </button>
+          </div>
         </div>
+      </div>
+
+      {/* MO */}
+      <div className="block lg:invisible z-50 fixed top-0 w-full fixed top-[50vh] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        {swiperOptions && !!imageListData.length ? (
+          <Swiper onSwiper={setSwiper} {...swiperOptions}>
+            {imageListData.map((image, idx) => (
+              <SwiperSlide key={idx}>
+                <Image src={image} alt="detail Image" />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
