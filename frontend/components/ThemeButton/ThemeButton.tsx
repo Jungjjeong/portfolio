ab34eebc695edb1058c5315dd4329c6c5d909ae9
Jungjjeong <sky990106@dgu.ac.kr>
2023-05-23 00:31:59 +0900

    feat: clientError 예외처리

--- frontend/components/ThemeButton/ThemeButton.tsx
+++ frontend/components/ThemeButton/ThemeButton.tsx
@@ -3,14 +3,17 @@
 import { useEffect, useState } from 'react';
 
 const ThemeButton = () => {
-  const [isDark, setIsDark] = useState<boolean>(false);
+  const [isDark, setIsDark] = useState<boolean>();
 
   useEffect(() => {
     const htmlDom = document.querySelector('html');
 
     if (htmlDom?.classList.contains('dark')) {
       setIsDark(true);
+      return;
     }
+
+    setIsDark(false);
   }, []);
 
   useEffect(() => {
@@ -30,28 +33,30 @@ const ThemeButton = () => {
 
   return (
     <>
-      <label className="flex cursor-pointer select-none items-center">
-        <div className="relative z-20">
-          <input
-            type="checkbox"
-            checked={isDark}
-            onChange={handleChange}
-            className="sr-only"
-          />
-          <div
-            className={`box block h-8 w-14 rounded-full ${
-              isDark ? 'bg-black/70' : 'bg-gray/70'
-            }`}
-          />
-          <div
-            className={`absolute left-1 top-1 flex h-6 w-6 items-center justify-center rounded-full bg-white transition text-4xl ${
-              isDark ? 'translate-x-full' : ''
-            }`}
-          >
-            {isDark ? '🌚' : '🌝'}
+      {isDark !== undefined && (
+        <label className="flex cursor-pointer select-none items-center">
+          <div className="relative z-20">
+            <input
+              type="checkbox"
+              checked={isDark}
+              onChange={handleChange}
+              className="sr-only"
+            />
+            <div
+              className={`box block h-8 w-14 rounded-full ${
+                isDark ? 'bg-black/70' : 'bg-gray/70'
+              }`}
+            />
+            <div
+              className={`absolute left-1 top-1 flex h-6 w-6 items-center justify-center rounded-full bg-white transition text-4xl ${
+                isDark ? 'translate-x-full' : ''
+              }`}
+            >
+              {isDark ? '🌚' : '🌝'}
+            </div>
           </div>
-        </div>
-      </label>
+        </label>
+      )}
     </>
   );
 };
